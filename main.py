b147f582266431e6f934f6e84ce721d8d8fd2fb1
bliblablub430 <luc.tiaden@students.lerbermatt.ch>
2025-09-16 18:45:48 +0200

    Gambling funktioniert fehlt aber noch was

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ clock = pygame.time.Clock()
 
 running = True
 game_state = "normal" # Die EINZIGE game_state Variable
+current_state = "waiting_for_bet" # für Roulette
 
 while running:
     
@@ -23,22 +24,14 @@ while running:
         game_state = Gambling.escaperoulette(game_state, event)
 
     # RUFE die rouletteloop auf und fange den neuen State auf
-    # Übergib das Characters-Objekt, damit die Funktion es bewegen kann
-    game_state = Gambling.rouletteloop(game_state)
+    game_state, current_state = Gambling.rouletteloop(game_state, current_state, events)
 
     # --- ZEICHNEN ---
-    # Dieser Block wird jetzt erreicht!
-    screen.fill((0, 0, 0)) # Hintergrund
-    
-    # Dieser Print wird jetzt funktionieren
-    print(f"Zeichne Trigger-Zone bei: {Gambling.roulette_trigger_zone}") 
-    
-    # Zeichne die Trigger-Zone (Rot)
+    screen.fill((0, 0, 0)) # Hintergrund 
     pygame.draw.rect(screen, (255, 0, 0), Gambling.roulette_trigger_zone)
-    
-    # Zeichne den Spieler (Blau)
     pygame.draw.rect(screen, (0, 0, 255), (Characters.x, Characters.y, Characters.h, Characters.l)) 
-    
+    if game_state == "roulette":
+        Gambling.roulettespiel_zeichnen(current_state, screen)
     pygame.display.update()
     clock.tick(60)
 
