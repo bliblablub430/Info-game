9f8692cb0624193d3c3eb854bc58d54fd95e0a9d
Olivier Bommeli <olivier.bommeli@students.lerbermatt.ch>
2025-09-16 12:15:09 +0200

    gliche commit wi luc wöu git am tweekä isch

--- Characters.py
+++ Characters.py
@@ -17,32 +17,32 @@ while running:
         if keys[pygame.K_ESCAPE]:
             running = False
 
-    def movement():
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_d]:
-            dx += 1
-        if keys[pygame.K_a]:
-            dx -= 1
-        if keys[pygame.K_w]:
-            dy -= 1
-        if keys[pygame.K_s]:
-            dy += 1
 
-        if dx or dy != 0:
-            vectorlen = (dx**2 + dy**2)**0.5
-            dx = dx/vectorlen
-            dy = dy/vectorlen 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        dx += 1
+    if keys[pygame.K_a]:
+        dx -= 1
+    if keys[pygame.K_w]:
+        dy -= 1
+    if keys[pygame.K_s]:
+        dy += 1
 
-        if keys[pygame.K_d]:
-            x += (dx*speed)
-        if keys[pygame.K_a]:
-            x += (dx*speed)
-        if keys[pygame.K_w]:
-            y += (dy*speed)
-        if keys[pygame.K_s]:
-            y += (dy*speed)       
+    if dx or dy != 0:
+        vectorlen = (dx**2 + dy**2)**0.5
+        dx = dx/vectorlen
+        dy = dy/vectorlen 
 
-        screen.fill((0,0,0))
-        pygame.draw.rect(screen, (0, 0, 255), (x, y, 50, 50))
-        pygame.display.update()
-        clock.tick(60)
+    if keys[pygame.K_d]:
+        x += (dx*speed)
+    if keys[pygame.K_a]:
+        x += (dx*speed)
+    if keys[pygame.K_w]:
+        y += (dy*speed)
+    if keys[pygame.K_s]:
+        y += (dy*speed)       
+
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (0, 0, 255), (x, y, 50, 50))
+    pygame.display.update()
+    clock.tick(60)
