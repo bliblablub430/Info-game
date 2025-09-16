407e6b5693a5a5338a37e0c2b9e74b9aa32ee50f
Trung Thông Pham <xo@xos-macbook-air.home>
2025-09-16 20:53:59 +0200

    Anleitung ergänzt und Code-Kommentare hinzugefügt

--- musik.py
+++ musik.py
@@ -2,13 +2,13 @@ import pygame
 
 pygame.mixer.init()
 
-def play_music(path, loop=True, volume=0.5):
+def play_music(path, loop=True, volume=0.5): #loop = True (Enlosschleife) und loop = False ( einmal abspielen)
     try:
-        pygame.mixer.music.load(path)
-        pygame.mixer.music.set_volume(volume)
-        pygame.mixer.music.play(-1 if loop else 0)
+        pygame.mixer.music.load(path) # Lädt die Musikdatei
+        pygame.mixer.music.set_volume(volume) # Setzt die Lautstärke
+        pygame.mixer.music.play(-1 if loop else 0)  # Startet die Wiedergabe (-1 = Endlosschleife, 0 = einmal)
     except pygame.error as e:
-        print(f"Fehler beim Laden der Musik: {e}")
+        print(f"Fehler beim Laden der Musik: {e}")  # Gibt eine Fehlermeldung aus, falls etwas schiefgeht
 
 def stop_music():
-    pygame.mixer.music.stop()
+    pygame.mixer.music.stop()  # Stoppt die aktuell laufende Musik
