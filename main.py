29d946e0f3aef04e746d10dde2b47a40679663a6
bliblablub430 <luc.tiaden@students.lerbermatt.ch>
2025-09-17 19:25:56 +0200

    Slots implementiert

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import Characters
 import musik
 import ostblock
 import mapinteraction
+import Slots
 
 pygame.init()
 pygame.mixer.init()
@@ -11,7 +12,7 @@ screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("Frau_Weidtmann_Hunter69")
 clock = pygame.time.Clock()
 
-almosteverything = mapinteraction.add_to_almosteverything([ostblock.wallcreation(), Gambling.roulette_trigger_zone, Characters.npc], mapinteraction.almosteverything)
+almosteverything = mapinteraction.add_to_almosteverything([ostblock.wallcreation(), Gambling.roulette_trigger_zone, Characters.npc, Slots.slot_trigger_zone], mapinteraction.almosteverything)
 
 # Player variables
 x, y = 700, 500
@@ -20,6 +21,7 @@ dx, dy = 0, 0
 running = True
 game_state = "normal" # Die EINZIGE game_state Variable
 current_state = "waiting_for_bet" # für Roulette
+slots_state = "auf_Start_warten" # für Slots
 musik.play_music("assets/sfx/main_theme.mp3", loop=True, volume=0.5)
 
 while running:
@@ -36,22 +38,30 @@ while running:
         
         # Events an die Funktionen übergeben, die sie brauchen
         game_state = Gambling.escaperoulette(game_state, event)
+        game_state = Slots.escapeslot(game_state, event)
 
     # RUFE die rouletteloop auf und fange den neuen State auf
     if game_state == "normal":
             x, y, dx, dy = mapinteraction.wallinteraction(x, y, dx, dy, almosteverything) #Wall collition plus Character Movement
             game_state,current_state = Gambling.rouletteloop(game_state, current_state, events)
+            game_state,slots_state = Slots.slotloop(game_state,slots_state, events)
             Characters.steps = Characters.npcmovement(Characters.npc, Characters.steps, Characters.npcspeed)
 
     elif game_state == "roulette":
          current_state = Gambling.roulettespiel_logik(current_state, events)
+
+    elif game_state == "slot":
+         slots_state = Slots.ergebnisseslot(slots_state, events)
     
     # --- ZEICHNEN ---
     screen.fill((255, 255, 255)) # Hintergrund
     pygame.draw.rect(screen, (255, 0, 0), Gambling.roulette_trigger_zone)
+    pygame.draw.rect(screen,(255,0,0),Slots.slot_trigger_zone)
     Characters.drawing(screen, Characters.character, Characters.npc, Characters.pixles) 
     if game_state == "roulette":
         Gambling.roulettespiel_zeichnen(current_state, screen)
+    elif game_state == "slot":
+         Slots.slotszeichnen(screen, slots_state, Slots.Ergebnisliste, Slots.Mitteilung)
     pygame.display.update()
     clock.tick(60)
 
