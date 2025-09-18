e295c643e82994fb4af9654d3f37604b7f614f5b
bliblablub430 <luc.tiaden@students.lerbermatt.ch>
2025-09-18 23:16:18 +0200

    ändläch ä Map

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("Frau_Weidtmann_Hunter69")
 clock = pygame.time.Clock()
 
-almosteverything = mapinteraction.add_to_almosteverything([ostblock.wallcreation(), Gambling.roulette_trigger_zone, Characters.npcs, Slots.slot_trigger_zone, black_jack.blackjack_trigger_zone], mapinteraction.almosteverything)
+almosteverything = mapinteraction.add_to_almosteverything([ostblock.wallcreation(), Gambling.roulette_trigger_zone, Characters.npcs, Slots.slot_trigger_zone, black_jack.blackjack_trigger_zone, ostblock.map_imgsp], mapinteraction.almosteverything)
 
 last_pixel = 0
 last_interaction = 0
@@ -84,6 +84,7 @@ while running:
 
      # --- ZEICHNEN ---
      screen.fill((255, 255, 255)) # Hintergrund
+     ostblock.drawmap(ostblock.map_imgup)
      pygame.draw.rect(screen, (255, 0, 0), Gambling.roulette_trigger_zone)
      last_pixel = Characters.drawing(screen, Characters.character, Characters.npcs, Characters.pixles, last_pixel)
      lives = herz_system.draw_lives(screen, lives)
